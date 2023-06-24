afa3d23cb2e66ef605391676f51575cb1c081818
AnandJNairGit <anandjnairit@gmail.com>
2023-06-24 14:31:05 +0530

    update mongo url

--- config/database.config.js
+++ config/database.config.js
@@ -1,4 +1,11 @@
-const url =
-  "mongodb+srv://nfjlabs:nfjlabs@nfjlabs.zxskiu9.mongodb.net/";
+// const url = "mongodb+srv://nfjlabs:nfjlabs@nfjlabs.zxskiu9.mongodb.net/";
 // "mongodb+srv://mehul:mehul123@team-478.xebuisz.mongodb.net/artisticNew?retryWrites=true&w=majority";
+// const url = "mongodb+srv://tester:Password01*@cluster0.tx0byb6.mongodb.net/?retryWrites=true&w=majority"
+// const url =
+//   "mongodb+srv://tester:Password01*@cluster0.2hnbjva.mongodb.net/?retryWrites=true&w=majority";
+const url =
+  "mongodb+srv://nfjlabs1:A6gcaUu7Pnlxedc2@nfjlabs1.2lanwtz.mongodb.net/?retryWrites=true&w=majority";
+
+// const url =
+//   "mongodb+srv://mehul:mehul123@team-478.xebuisz.mongodb.net/artisticNew?retryWrites=true&w=majority";
 export default url;
